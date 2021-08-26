0507e6fb69d3c0a70dd280936a90a8e28caa97d9
Lucas Mezencio <lucasmezencio@outlook.com>
2021-08-26 15:38:27 -0300

    implement map and show marker on new york

--- app/src/main/java/com/lucasmezencio/locationbootcamp/MapsActivity.kt
+++ app/src/main/java/com/lucasmezencio/locationbootcamp/MapsActivity.kt
@@ -1,20 +1,28 @@
 package com.lucasmezencio.locationbootcamp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.lucasmezencio.locationbootcamp.databinding.ActivityMapsBinding
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(),
+        OnMapReadyCallback,
+        GoogleMap.OnMarkerClickListener{
 
-    private lateinit var mMap: GoogleMap
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var binding: ActivityMapsBinding
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,11 +46,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        map = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val myPlace = LatLng(40.73, -73.99)
+        map.addMarker(MarkerOptions().position(myPlace).title("Minha cidade favorita"))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(myPlace, 12.0f))
+
+        map.uiSettings.isZoomControlsEnabled = true
+        map.setOnMarkerClickListener(this)
     }
+
+
+    override fun onMarkerClick(p0: Marker): Boolean = false
 }
